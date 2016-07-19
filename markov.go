6bdb298ce6e3c9cc38c2d7ca039d50ceba58611f
Alex Nichol <unixpickle@gmail.com>
2016-07-19 14:17:52 -0400

    improve Markov predictions
    There were two factors in this improvement. First, lookahead was added, so the word *after* the next word is taken into account while picking the next word. Second, single-letter words are not allowed besides words like "a" and "I".

--- markov.go
+++ markov.go
@@ -7,11 +7,15 @@ import (
 	"strings"
 )
 
+var markovSingleLetterWords = []string{"a", "I"}
+
 const maxWordLen = 20
 
 // Markov is a Splicer that uses a simple Markov chain
 // to insert spaces into a piece of text.
 type Markov struct {
+	TotalCount int
+
 	// RawCounts stores the number of occurrences of each
 	// word in the training corpus. It can be thought of
 	// as a frequency map of underlying words.
@@ -22,14 +26,22 @@ type Markov struct {
 	// as the frequency map of words given that a different
 	// word was just seen.
 	Table map[string]map[string]int
+
+	// TableCounts stores the total number of table entries
+	// for each word (i.e. it is the sum of the values for
+	// each key in each table). This may be different than
+	// RawCounts, since RawCounts includes occurrences at
+	// the very ends of training documents.
+	TableCounts map[string]int
 }
 
 // TrainMarkov trains a Markov model on a directory full
 // of sample text files.
 func TrainMarkov(corpusDir string) (*Markov, error) {
 	res := &Markov{
-		RawCounts: map[string]int{},
-		Table:     map[string]map[string]int{},
+		RawCounts:   map[string]int{},
+		Table:       map[string]map[string]int{},
+		TableCounts: map[string]int{},
 	}
 	contents, err := ioutil.ReadDir(corpusDir)
 	if err != nil {
@@ -60,32 +72,69 @@ func DeserializeMarkov(d []byte) (*Markov, error) {
 	return &res, nil
 }
 
-// MostLikely returns the most likely word to be
-// observed, given the previous word. If there was
-// no previous word, it should be the empty string.
-func (m *Markov) MostLikely(previous string, options []string) string {
-	var bestCount, bestIdx int
-	for i, option := range options {
-		if nextMap := m.Table[previous]; nextMap != nil {
-			count := m.Table[previous][option]
-			if count > bestCount {
-				bestCount = count
-				bestIdx = i
-			}
-		}
+// CondProb returns the conditional probability of a
+// word given the previous word. It returns 0 if the
+// word has never been seen after the previous word.
+// It returns 1 if the word is "".
+func (m *Markov) CondProb(previous, word string) float64 {
+	if word == "" {
+		return 1
 	}
-	if bestCount > 0 {
-		return options[bestIdx]
+	prevMap := m.Table[previous]
+	if prevMap == nil {
+		return 0
+	}
+	nextCount := m.TableCounts[previous]
+	return float64(prevMap[word]) / float64(nextCount)
+}
+
+// Prob returns the unconditional probability of a word.
+// It returns 0 if the word has never been seen before.
+// It returns 1 if the word is "".
+func (m *Markov) Prob(word string) float64 {
+	if word == "" {
+		return 1
+	}
+	return float64(m.RawCounts[word]) / float64(m.TotalCount)
+}
+
+// BestField returns the most likely next field in the
+// string given the previous field.
+//
+// The previous field "" means this is the first field.
+func (m *Markov) BestField(previous string, str string) string {
+	var bestCond float64
+	var bestCondStr string
+	var bestUncond float64
+	var bestUncondStr string
+
+	followingPairs(str, func(w1, w2 string) {
+		condProb := m.CondProb(previous, w1) * m.CondProb(w1, w2)
+		if condProb > bestCond {
+			bestCond = condProb
+			bestCondStr = w1
+		}
+		uncondProb := m.Prob(w1) * m.Prob(w2)
+		if uncondProb > bestUncond {
+			bestUncond = uncondProb
+			bestUncondStr = w1
+		}
+	})
+
+	if bestCond > 0 {
+		return bestCondStr
+	} else if bestUncond > 0 {
+		return bestUncondStr
 	}
 
-	for i, option := range options {
-		count := m.RawCounts[option]
-		if count > bestCount {
-			bestCount = count
-			bestIdx = i
+	followingWords(str, func(w1 string) {
+		p := m.Prob(w1)
+		if p >= bestUncond {
+			bestUncond = p
+			bestUncondStr = w1
 		}
-	}
-	return options[bestIdx]
+	})
+	return bestUncondStr
 }
 
 // Fields uses the Markov model to split the spaceless
@@ -96,13 +145,8 @@ func (m *Markov) Fields(text string) []string {
 	for _, part := range parts {
 		var lastWord string
 		for i := 0; i < len(part); i += len(lastWord) {
-			followingWords := make([]string, maxWordLen)
-			for l := 1; l <= maxWordLen && l+i <= len(part); l++ {
-				followingWords[l-1] = part[i : i+l]
-			}
-			lastWord = m.MostLikely(lastWord, followingWords)
+			lastWord = m.BestField(lastWord, part[i:])
 			res = append(res, lastWord)
-
 		}
 	}
 	return res
@@ -121,17 +165,46 @@ func (m *Markov) Serialize() ([]byte, error) {
 }
 
 func trainMarkovSample(m *Markov, sampleFields []string) {
-	for i, field := range sampleFields {
-		m.RawCounts[field]++
-		var lastWord string
-		if i > 0 {
-			lastWord = sampleFields[i-1]
+	var lastWord string
+	for _, field := range sampleFields {
+		if len(field) == 1 {
+			var allowed bool
+			for _, x := range markovSingleLetterWords {
+				if x == field {
+					allowed = true
+				}
+			}
+			if !allowed {
+				continue
+			}
 		}
+		m.TotalCount++
+		m.RawCounts[field]++
+		m.TableCounts[lastWord]++
 		subTable := m.Table[lastWord]
 		if subTable == nil {
 			subTable = map[string]int{}
 			m.Table[lastWord] = subTable
 		}
 		subTable[field]++
+		lastWord = field
+	}
+}
+
+func followingPairs(str string, f func(w1, w2 string)) {
+	followingWords(str, func(w1 string) {
+		if len(w1) == len(str) {
+			f(w1, "")
+		} else {
+			followingWords(str[len(w1):], func(w2 string) {
+				f(w1, w2)
+			})
+		}
+	})
+}
+
+func followingWords(str string, f func(string)) {
+	for l := 1; l <= maxWordLen && l <= len(str); l++ {
+		f(str[:l])
 	}
 }
